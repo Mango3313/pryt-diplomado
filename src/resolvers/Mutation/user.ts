7c0ac3077ebe3de068fdd8e177c9fce7779d2b49
Mango3313 <daft_noob3313@live.com>
2021-09-20 21:33:57 -0500

    feat: correccion de errores, problema con duplicado grahpql
    -quitar modelos para rediseñar schemas

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from "bcryptjs";
 import * as randomstring from "randomstring";
-import { UserCreateInput } from "../../generated/prisma-client";
+import { UserCreateInput } from "../../generated/prisma-client"
 import { Context } from "../../utils";
 
 export default {
@@ -11,9 +11,12 @@ export default {
   },
   updateUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.updateUser(args),
   createUser: (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(data.password,salt);
     const token = randomstring.generate(8).toUpperCase();
     data = {
       token,
+      password,
       ...data
     };
     return ctx.prisma.createUser(data);
